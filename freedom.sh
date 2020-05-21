17738bdccda35dece2893d92a779c8f14b4bf9eb
skycar8 <64244128+skycar8@users.noreply.github.com>
2020-05-21 19:33:55 +0800

    Update freedom.sh

--- freedom.sh
+++ freedom.sh
@@ -226,7 +226,31 @@ function installV2ray(){
     uuid=$(curl https://www.uuidgenerator.net/api/version4)
     purple "uuid: $uuid"
     
-    
+    yellow ">>>>>>>> nginx添加对v2ray的监听"
+    sudo cat >> /etc/nginx/sites-available/$1.conf <<-EOF
+    server {
+        listen 127.0.0.1:80;
+        server_name $1;
+
+        location /free { #与 V2Ray 配置中的 path 保持一致
+            if (\$http_upgrade != "websocket") { #WebSocket协商失败时返回404
+                return 404;
+            }
+            proxy_redirect off;
+            proxy_pass http://127.0.0.1:10001; #假设WebSocket监听在环回地址的10001端口上
+            proxy_http_version 1.1;
+            proxy_set_header Upgrade \$http_upgrade;
+            proxy_set_header Connection "upgrade";
+            proxy_set_header Host \$http_host;
+
+            # Show realip in v2ray access.log
+            proxy_set_header X-Real-IP \$remote_addr;
+            proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+        }
+    }
+    EOF
+    echo "/etc/nginx/sites-available/$1.conf"
+    cat /etc/nginx/sites-available/$1.conf
     
     yellow ">>>>>>>> 重启nginx"
     systemctl restart nginx
