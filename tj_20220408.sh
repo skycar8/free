b9642ae5f90272d44096afe5c9822dba32565d51
skycar8 <64244128+skycar8@users.noreply.github.com>
2022-04-08 14:14:15 +0800

    Update tj_20220408.sh

--- tj_20220408.sh
+++ tj_20220408.sh
@@ -16,7 +16,7 @@ function installCert(){
 	~/.acme.sh/acme.sh  --installcert  -d  $1   \
         --key-file   /etc/nginx/ssl/$1.key \
         --fullchain-file /etc/nginx/ssl/$1.crt \
-        --reloadcmd  "service nginx force-reload" \
+        --reloadcmd  "sudo service nginx force-reload" \
         --ecc
 }
 
